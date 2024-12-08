caa2c4f1ac96a8828796c235e091349cded56048
ianmuchyri <ianmuchiri8@gmail.com>
2024-12-08 20:44:26 +0300

    add component
    Signed-off-by: ianmuchyri <ianmuchiri8@gmail.com>

---
+++ src/components/component.tsx
@@ -0,0 +1,9 @@
+export default function PublicComponent() {
+  return (
+    <div>
+      <h2 className="font-semibold text-xl">
+        This is the public repo component
+      </h2>
+    </div>
+  );
+}
